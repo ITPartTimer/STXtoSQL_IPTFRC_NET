8fd49b2e89fd8960434e925fc3b90a58ae9ed76f
Scott Clemons <scott.clemons317@outlook.com>
2020-06-11 14:48:10 -0600

    Add project files.

---
+++ STXtoSQL_IPTFRC_NET/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STXtoSQL_IPTFRC_NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
